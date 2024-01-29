f690980d2ac5680c3f8f8f30fbca312ee2bd9526
Peter_Wu <changeya@yahoo.com.tw>
2024-01-29 19:06:26 +0800

    加入專案檔案。

---
+++ MSIT155Site/Controllers/ApiController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MSIT155Site.Models;
+
+namespace MSIT155Site.Controllers
+{
+    public class ApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        private readonly MyDBContext _context;
+        public ApiController(MyDBContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Cities()
+        {
+            var cities = _context.Addresses.Select(p => p.City).Distinct();
+            return Json(cities);
+        }
+
+
+        public IActionResult Card()
+        {
+            return View();
+        }
+
+
+    }
+}
